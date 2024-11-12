9bd93c859d20106b9dc692d2359bf48e7de55b81
Kosta Kai Ugrina <kkugrina@gmail.com>
2024-11-12 18:47:37 +0100

    fix json serializer

---
+++ RouteFinder.API/Utils/Serialization/GoogleApiEnumMemberAttributeStringConverter.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Runtime.Serialization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RouteFinder.API.Utils.Serialization
+{
+    public class GoogleApiEnumMemberAttributeStringConverter : JsonConverter<object?>
+    {
+
+
+        //public override void WriteJson(JsonWriter writer, object? value, JsonSerializer serializer)
+        //{
+        //    if (value == null)
+        //    {
+        //        writer.WriteNull();
+        //        return;
+        //    }
+
+        //    string? enumStringValue = ExtractEnumMemberAttributeValue(value as Enum);
+        //    writer.WriteValue(enumStringValue);
+        //}
+
+        private static string ExtractEnumMemberAttributeValue(Enum e)
+        {
+            var enumType = e.GetType();
+            var memberInfos = enumType.GetMember(e.ToString());
+            var enumValueMemberInfo = memberInfos.FirstOrDefault(m => m.DeclaringType == enumType);
+            var enumMember = enumValueMemberInfo
+                .GetCustomAttributes(typeof(EnumMemberAttribute), false)[0] as EnumMemberAttribute;
+            var enumMemberValue = enumMember.Value;
+
+            return enumMemberValue;
+        }
+
+        public override bool CanConvert(Type typeToConvert)
+            => (Nullable.GetUnderlyingType(typeToConvert) ?? typeToConvert).IsEnum;
+
+        public override object? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, object? value, JsonSerializerOptions options)
+        {
+            if (value == null)
+            {
+                writer.WriteNullValue();
+                return;
+            }
+            string? enumStringValue = ExtractEnumMemberAttributeValue(value as Enum);
+            writer.WriteStringValue(enumStringValue);
+        }
+    }
+}
--- RouteFinder.API/Utils/Serialization/GoogleApiJsonConverter.cs
+++ RouteFinder.API/Utils/Serialization/GoogleApiJsonConverter.cs
@@ -1,19 +1,33 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
+﻿//using Newtonsoft.Json;
+//using Newtonsoft.Json.Serialization;
+
+using System.Text.Json;
 
 namespace RouteFinder.API.Utils.Serialization
 {
     public static class GoogleApiJsonConverter
     {
+
         public static string Serialize(object obj)
-            => JsonConvert.SerializeObject(
-                value: obj,
-                settings: new JsonSerializerSettings
-                {
-                    Converters = [
-                        new GoogleApiEnumUppercaseStringConverter(),
-                        new GoogleApiJsonPropertyNamesConverter()],
-                    ContractResolver = new CamelCasePropertyNamesContractResolver(),
-                });
+        {
+            var options = new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            };
+            options.Converters.Add(new GoogleApiEnumMemberAttributeStringConverter());
+            var jsonObj = JsonSerializer.Serialize(obj, options);
+            return jsonObj;
+        }
+
+        //public static string Serialize(object obj)
+        //    => JsonConvert.SerializeObject(
+        //        value: obj,
+        //        settings: new JsonSerializerSettings
+        //        {
+        //            Converters = [
+        //                new GoogleApiEnumUppercaseStringConverter(),
+        //                new GoogleApiJsonPropertyNamesConverter()],
+        //            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+        //        });
     }
 }
